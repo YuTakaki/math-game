8b2067140bf35c61239c78a0273555c4a08f8551
YuTakaki <yu.takaki2000@gmail.com>
2020-09-09 22:04:01 +0900

    first commit9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Suspense} from 'react';
 import './styles/css/main.css';
 import {HashRouter as Router, Route} from 'react-router-dom';
+import ScoreList from './components/scoreList';
+import Game from './components/game';
+import Menu from './components/menu';
 
-const ScoreList = React.lazy(()=>import('./components/scoreList'));
-const Game = React.lazy(()=>import('./components/game'))
-const Menu = React.lazy(() => import('./components/menu'));
+// const ScoreList = React.lazy(()=>import('./components/scoreList'));
+// const Game = React.lazy(()=>import('./components/game'))
+// const Menu = React.lazy(() => import('./components/menu'));
 class App extends React.Component {
   constructor(props){
     super(props)
@@ -17,11 +20,11 @@ class App extends React.Component {
       <Router>
         <div className="App">
           <h1>Math Game</h1>
-          <Suspense fallback={<div>Wait...</div>}>
+          {/* <Suspense fallback={<div>Wait...</div>}> */}
             <Route exact path='/' component={Menu}/>
             <Route path='/game' component={Game}/>
             <Route path='/scoreList' component={ScoreList}/>
-          </Suspense>
+          {/* </Suspense> */}
         </div>
       </Router>
     );
